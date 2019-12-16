0576a422d2bb6a957ee59fe84a4b257f60417e11
Andrew Weller <andrewweller.cs@gmail.com>
2019-12-16 11:16:18 -0700

    Stage3: load into project

---
+++ stage3/stage3.h
@@ -0,0 +1,11 @@
+#ifndef STAGE3_H
+#define STAGE3_H
+
+#include "graphics/Shader.h"
+#include "graphics/Mesh.h"
+#include "graphics/Texture.h"
+#include "graphics/Camera.h"
+
+#include "Window.h"
+
+#endif
